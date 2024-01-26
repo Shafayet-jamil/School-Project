3c57a6868b001fedf34e02868907464307b730da
Shafayet Jamil <144683352+Shafayet-jamil@users.noreply.github.com>
2024-01-26 18:42:34 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+const button = document.querySelector('frame-12-mmN');
+    
+<div class="auto-group-xakl-zig">
+     
+    <div class="auto-group-sbsu-vcL">
+        <div class="rectangle-25-SKn">
+        </div>
+        <p class="modern-psychology-k5a">Modern Psychology</p>
+        <p class="tk-FY8">1300 TK</p>
+        <div class="frame-12-mmN">Buy Course</div>
+        <div class="auto-group-uoik-d2t">
+          <img class="calendar-outline-4-MUg" src="./assets/calendar-outline-4.png" />
+          <p class="start-20-april-2021-Ht8">Start 20 April, 2021</p>
+          <img class="people-outline-4-252" src="./assets/people-outline-4.png" />
+          <p class="seats-jVE">60 seats</p>
+        </div>
+      </div>
+      <div class="auto-group-x148-R7A">
+        <div class="auto-group-gfdn-7kg">
+          <div class="rectangle-26-G7n">
+          </div>
+        </div>
+        <p class="modern-psychology-vCL">Modern Psychology</p>
+        <p class="tk-324">1300 TK</p>
+        <div class="frame-13-MoS">Buy Course</div>
+        <div class="auto-group-dnyt-S4C">
+          <img class="calendar-outline-5-AVz" src="./assets/calendar-outline-5.png" />
+          <p class="start-20-april-2021-VoA">Start 20 April, 2021</p>
+          <img class="people-outline-5-pqS" src="./assets/people-outline-5.png" />
+          <p class="seats-MaU">60 seats</p>
+        </div>
+      </div>
+    
+    </div>
+
+</div>
+    
+
+        <div class="auto-group-mvip-rXE">
+            <div class="auto-group-4tcg-ZRe">
+            <div class="rectangle-35-6Ra">
+            </div>
+            <div class="auto-group-3ama-DWC">
+                <p class="modern-psychology-yVN">Modern Psychology</p>
+                <p class="tk-6K6">1300 TK</p>
+            </div>
+            </div>
+            <div class="auto-group-h5nk-pkt">
+            <div class="rectangle-36-kPe">
+            </div>
+            <div class="auto-group-i7ks-sjA">
+                <div class="auto-group-zb6c-cwe">
+                <p class="modern-psychology-MuE">Modern Psychology</p>
+                <p class="tk-ssa">1300 TK</p>
+                </div>
+                <img class="icon-outlined-suggested-symbol-x-RPJ" src="./assets/icon-outlined-suggested-symbol-x-dKa.png" />
+            </div>
+            </div>
+            <div class="auto-group-cmun-wcY">
+            <div class="rectangle-37-rDi">
+            </div>
+            <div class="auto-group-92kv-afW">
+                <div class="auto-group-f6bn-Ksz">
+                <p class="modern-psychology-4Kn">Modern Psychology</p>
+                <p class="tk-bKi">1300 TK</p>
+                </div>
+                <img class="icon-outlined-suggested-symbol-x-8Ke" src="./assets/icon-outlined-suggested-symbol-x-pha.png" />
+            </div>
+            </div>
+            <div class="auto-group-26ag-rFe">
+            <div class="rectangle-38-aBe">
+            </div>
+            <div class="auto-group-fzja-6vg">
+                <div class="auto-group-i2ul-4Mi">
+                <p class="modern-psychology-DEc">Modern Psychology</p>
+                <p class="tk-kkL">1300 TK</p>
+                </div>
+                <img class="icon-outlined-suggested-symbol-x-udE" src="./assets/icon-outlined-suggested-symbol-x-yQG.png" />
+            </div>
+            </div>
+            <div class="line-1-S7N">
+            </div>
+            <div class="auto-group-st3r-Yw6">
+            <div class="auto-group-tbve-tEG">
+                <p class="sub-total-dhe">Sub-Total</p>
+                <p class="items-Ypc">4 Items</p>
+            </div>
+            <p class="tk-f8Y">5312 TK</p>
+            </div>
+            <div class="frame-19-zwW">Checkout</div>
+        </div>
+   
